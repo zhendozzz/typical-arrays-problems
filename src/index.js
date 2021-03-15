b779090a469464665acb68e3ec5f617c77fb162e
zhendozzz <info@zhendozzz.ru>
2021-03-15 16:26:28 +0300

    init: initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    let min = 0;
+    if (Array.isArray(array)) {
+        array.forEach(element => {
+            if (min > element) {
+                min = element;
+            }
+        });
+    }
+    return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    let max = 0;
+    if (Array.isArray(array)) {
+        array.forEach(element => {
+            if (max < element) {
+                max = element;
+            }
+        });
+    }
+    return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    let avg = 0;
+    if (Array.isArray(array) && (array.length != 0)) {
+        let sum = array.reduce((accum, value) => {
+            return accum + value;
+        }, 0);
+        avg = sum / array.length;
+    }
+    return avg;
 }
